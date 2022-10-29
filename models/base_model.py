74e81137024bcc4d11f74b9998db5da2427411df
Wachira007 <dannielmutua00@gmail.com>
2022-10-29 04:23:33 -0700

    Generating an object from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -10,13 +10,22 @@ class BaseModel:
     """
     defines all common attributes/methods for other classes
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Instance initialization
+	self and arguments
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+		for key, value in kwargs.items():
+			if key == "created_at" or key == "updated_at":
+				value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+			if key == "__class__":
+				continue
+			setattr(self, key, value)
+	else:
+		self.id = str(uuid.uuid4())
+        	self.created_at = datetime.now()
+        	self.updated_at = datetime.now()
 
     def __str__(self):
         """Instance string format"""
